d473c52bf873b89d40f1b78344a3e646f4238246
XieWei <wuxiaoshen@shu.edu.cn>
2019-10-21 10:06:29 +0800

    :rocket: 12306

--- src/Railway12306/settings.go
+++ src/Railway12306/settings.go
@@ -7,9 +7,16 @@ import (
 
 var STATION_URL = "https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9114"
 var trainUrl = "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=%s&leftTicketDTO.from_station=%s&leftTicketDTO.to_station=%s&purpose_codes=%s"
+var priceUrl = "https://kyfw.12306.cn/otn/leftTicket/queryTicketPrice?train_no=%s&from_station_no=%s&to_station_no=%s&seat_types=%s&train_date=%s"
+var railwayUrl = "https://kyfw.12306.cn/otn/queryTrainInfo/query?leftTicketDTO.train_no=67000G600401&leftTicketDTO.train_date=2019-10-25&rand_code="
+var search = "https://search.12306.cn/search/v1/train/search?keyword=G60&date=20191020"
 
 func RailWayURL(date string, fromStation string, toStation string, codes string) string {
 	return fmt.Sprintf(trainUrl, date, strings.ToUpper(fromStation), strings.ToUpper(toStation), strings.ToUpper(codes))
+}
+
+func RailWayPrice(number string, date string, fromStationNo string, toStationNo string, seatTypes string) string {
+	return fmt.Sprintf(priceUrl, number, fromStationNo, toStationNo, seatTypes, date)
 }
 
 var TickType map[int]string
