faaf200c83b1365391a7ead85beca02e659718f5
XieWei <wuxiaoshen@shu.edu.cn>
2019-10-25 15:01:07 +0800

    :rocket: v0.1

---
+++ src/weixin/settings.go
@@ -0,0 +1,7 @@
+package weixin
+
+var (
+	HOST  = "https://weixin.sogou.com/"
+	HOT   = "https://weixin.sogou.com/pcindex/pc/pc_0/1.html"
+	SMILE = "https://weixin.sogou.com/pcindex/pc/pc_1/pc_1.html"
+)
