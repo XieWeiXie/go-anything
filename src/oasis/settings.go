02c6fac7e3f891743c13e87524fc5226e083f1e6
XieWei <wuxiaoshen@shu.edu.cn>
2019-10-28 00:24:34 +0800

    :rocket: oasis

---
+++ src/oasis/settings.go
@@ -0,0 +1,19 @@
+package oasis
+
+/*
+微博清爽社交平台：绿洲 接口
+*/
+
+var (
+	WOW      = "https://oasis.weibo.cn/v1/wow/list" // 用户信息
+	PROFILE  = "https://oasis.weibo.cn/v1/user/profile"
+	TIMELINE = "https://oasis.weibo.cn/v1/timeline/user"
+	DISCOVER = "https://oasis.weibo.cn/v1/timeline/discovery"
+)
+
+var (
+	Star    = 16 // 明星
+	Face    = 11 // 颜值
+	Photo   = 18 // 摄影
+	Fashion = 6 // 时尚
+)
