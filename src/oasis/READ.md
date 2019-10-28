235a80e262b3b4257ac206096bdaafc43aaa8f41
XieWei <wuxiaoshen@shu.edu.cn>
2019-10-28 17:41:42 +0800

    :rocket: oasis

---
+++ src/oasis/READ.md
@@ -0,0 +1,18 @@
+# 分析过程
+
+uid: 用户唯一id
+
+几乎所有的请求带有这几个URL参数：
+- ua: smartisan-OD103__oasis__1.4.17__Android__Android7.1.1
+- wm: 9009_90025
+- aid: 01A0QclqWHjYDiPccIVGsKkq8VuZpCUIaUQOAW6PCht1I4BUo.
+- cuid: uid
+- sign: a4f4d750343e113b29d71b923c300f12 // 变动: md5 32位 || 难点 gsid
+- platform: ANDROID
+- cfrom: 2899595010 
+- version: 1.4.17 // 绿洲 app 版本
+- timestamp: 1572242797242 // 时间戳
+- noncestr: w99QX4s989sn6o0fdd810I2O0Z3G55 // 变动: 随机字符 30 位
+
+Header：
+- gsid: rWf/skaVKcSmS1v+WmGQmFFI5rCwUfhG7kx9FomaSbnUzxZZw6ViLbReM6RRMZRY5d8XhiG7PlDBK+wW6ApbU2dxl1gxx9OX41NSAbEqKIONpCIgJM9FBYKAypZZjwxQ
--- src/oasis/settings.go
+++ src/oasis/settings.go
@@ -9,11 +9,62 @@ var (
 	PROFILE  = "https://oasis.weibo.cn/v1/user/profile"
 	TIMELINE = "https://oasis.weibo.cn/v1/timeline/user"
 	DISCOVER = "https://oasis.weibo.cn/v1/timeline/discovery"
+	Upload   = "https://picupload.weibo.com/interface/upload.php?appid=3157701306&cs=3588630082&ent=xiancheng&file_source=53&gsid=_2A9Y6hVryDIVOw5wYhtwUazlgUavKDdCBN9YkoHznUhyKSZN6hPZZsD-ykK3SLhoo4M4sybT2OFwCyfIMAIPJgxAu&ori=1&uid=1948244870"
+	Create   = "https://oasis.weibo.cn/v1/status/create"
 )
 
 var (
 	Star    = 16 // 明星
 	Face    = 11 // 颜值
 	Photo   = 18 // 摄影
-	Fashion = 6 // 时尚
+	Fashion = 6  // 时尚
 )
+
+/*
+绿洲 API
+- status:
+	/v1/status/destroy
+	/v1/status/create
+	/v1/status/detail
+- at
+	/v1/status/at/statuses
+- like
+	/v1/status/like/statuses
+- timeline
+	/v1/timeline/user
+	/v1/timeline/following
+	/v1/timeline/discovery
+- report
+	/v1/report/stats
+- h5
+	/v1/h5/share
+- user
+	/v1/user/profile
+	/v1/user/star
+	/v1/user/following_recommend
+	/v1/user/logout
+	/v1/user/captcha
+	/v1/user/login
+- wow
+	/v1/wow/list
+- sso
+	/v1/sso/mfp
+- message
+	/v1/message/list
+- search
+	/v1/search/home
+- friend_chain
+	/v1/friend_chain/feed_user_recommends
+- channel
+	/v1/channel/list
+- location
+	/v1/location/nearby
+- comment
+	/v1/comment/create
+
+- app 版本:https://ota.weibo.cn
+	/api/app/version/upgrade
+- 图片上传: https://picupload.weibo.com // https://picupload.service.weibo.com
+	/interface/upload.php
+
+*/
