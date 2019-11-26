bb60185455713d91908bd9ce0170fe5aacf2a09c
XieWei <wuxiaoshen@shu.edu.cn>
2019-11-26 22:04:09 +0800

    :rocket: v0.5

--- cmd/root.go
+++ cmd/root.go
@@ -3,10 +3,6 @@ package cmd
 import (
 	"log"
 
-	kafka_operator "github.com/wuxiaoxiaoshen/go-anything/pkg/kafka"
-
-	redis_operator "github.com/wuxiaoxiaoshen/go-anything/pkg/redis"
-
 	"github.com/wuxiaoxiaoshen/go-anything/pkg/mysql"
 
 	"github.com/wuxiaoxiaoshen/go-anything/router"
@@ -22,10 +18,10 @@ var ROOT = &cobra.Command{
 		mysql_operator.MySQLInit()
 
 		log.Println("Step 2: Redis...")
-		redis_operator.RedisInit()
+		//redis_operator.RedisInit()
 
 		log.Println("Step 3: Kafka...")
-		kafka_operator.KafkaInit()
+		//kafka_operator.KafkaInit()
 
 	},
 	Run: func(cmd *cobra.Command, args []string) {
@@ -35,9 +31,9 @@ var ROOT = &cobra.Command{
 		log.Println("End Step 0: Mysql close...")
 		defer mysql_operator.DefaultMySQLAction.Close()
 		log.Println("End Step 1: Redis close...")
-		defer redis_operator.DefaultRedisAction.Close()
+		//defer redis_operator.DefaultRedisAction.Close()
 		log.Println("End Step 2: Kafka close...")
-		defer kafka_operator.DefaultAsyncProducer.Close()
+		//defer kafka_operator.DefaultAsyncProducer.Close()
 	},
 }
 
